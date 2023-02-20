038c56c8392ed5efe1bf5cffd4f0beb98c573993
ragnar177 <cc925721@gmail.com>
2023-02-20 13:36:56 -0500

    fisrt_activity

---
+++ activity_one.py
@@ -0,0 +1,137 @@
+"""
+#Cajero electrónico
+tex_one = "welcome to server" #str
+
+variable = "saldo" #str
+
+saldo = 59000.79 #FLOAT
+
+tex_two = "cuanto deseas retirar" #str
+
+valor_retirar = 25000 #FLOAT
+
+saldo_final = "su saldo actual es" #str
+
+resta = saldo - valor_retirar
+
+
+print(tex_one)
+print(variable)
+print(saldo)
+print(tex_two)
+print(valor_retirar)
+print(saldo_final)
+print(resta)
+
+"""
+"""
+#Usar chat
+
+Text_one = "Estado del amigo" #str
+
+variable1 = "En línea" #str
+
+variable2 = "mensaje" #str
+
+variable3 = "Hola, cómo está?" #str
+
+variable4 = "respuesta" #str
+
+variable5 = "Muy bien, y tú?" #str
+
+print (Text_one)
+print (variable1)
+print (variable2)
+print (variable3)
+print (variable4)
+print (variable5)
+
+"""
+"""
+
+tex_one = "welcome to store" #str
+
+variable = "Costo de compra" #str
+ 
+variable1 = 30000 #INT
+
+variable2 = "Saldo" #str
+
+saldo = 100000 #INT
+
+tex_two = "Cambio" #str
+
+valor_retirar = saldo - variable1
+
+
+print(tex_one)
+print(variable)
+print(variable1)
+print(variable2)
+print(saldo)
+print(tex_two)
+print(valor_retirar)
+
+"""
+
+"""
+
+Text_one = "Welcome to fresh" #str
+
+variable1 = "La capacidad de esta lavadora es de:" #str
+
+variable2 = "25 lb" #str
+
+variable3 = "Inserte cantidad de ropa" #str
+
+variable4 = "10 prendas" #str
+
+variable5 = "Hora inicio" #str
+
+variable6 = "18:45" #date
+
+variable7 = "fecha fin" #str
+
+variable8 = "19:35" #date
+
+print (Text_one)
+print (variable1)
+print (variable2)
+print (variable3)
+print (variable4)
+print (variable5)
+print (variable6)
+print (variable7)
+print (variable8)
+
+"""
+
+Text_one = "Verifica tu saldo" #str
+
+variable1 = "Inserte código" #str
+
+variable2 = "*10#" #str
+
+variable7 = "tu saldo" #str
+
+variable8 = 5000.55 #FLOAT
+
+variable3 = "Inserte número" #str
+
+variable4 = 3023819230 #INT
+
+variable5 = "llamando..." #str
+
+variable6 = "sistema correo de voz, tendrá costo a partir del tono (suena)" #str
+
+print (Text_one)
+print (variable1)
+print (variable2)
+print (variable7)
+print (variable8)
+print (variable3)
+print (variable4)
+print (variable5)
+print (variable6)
+
+
