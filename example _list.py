46c957e10f5701d9027f23c1b1fe1c2ffd8cb40e
ragnar177 <cc925721@gmail.com>
2023-02-22 09:05:20 -0500

    list

---
+++ example _list.py
@@ -0,0 +1,25 @@
+"""
+#list
+name = ['jimmy','duber','john k','ander']
+
+print(name)
+
+"""
+
+"""
+#data of person
+#diccionari
+data = {
+    "name": "andres",
+    "years_old": 19
+    }
+
+print(data)
+
+"""
+#subir a github
+
+
+
+
+
