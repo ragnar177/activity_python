ddc66d606734ed192763ca8de4867b795c60fca8
ragnar177 <cc925721@gmail.com>
2023-03-01 11:47:29 -0500

    examples

--- sueldo_ABC.py
+++ sueldo_ABC.py
@@ -1,19 +1,17 @@
 
 text_1 = "es momento de determinar el salario de los empleados de los poryectos A, B y C"
 print(text_1)
-
 text_2 = "nombre del empleado:"
 print(text_2)
-name = "andres salguero"
-print(name)
+name = input()
 text_3 = "documento del empleado:"
 print(text_3)
-numero_documento = 1000537353
-print(numero_documento)
+numero_documento = input()
 text_4 = "proyecto al que pertenece el empleado:"
 print(text_4)
-unicos_proyectos = 1 and 2 and 3
-proyecto_1 = 3
+unicos_proyectos = 1
+unicos_proyectos1 = 3
+proyecto_1 = 1
 
 lista_proyectos = {
     'P_A': 20000,
@@ -27,16 +25,16 @@ salario_B = 30 * (lista_proyectos ['P_B'] * 8)
 
 salario_C = (30 * (lista_proyectos ['P_C'] * 8)) + (((lista_proyectos ['P_C'] * (6/100)) + lista_proyectos ['P_C']) * 3)
 
-if proyecto_1 == unicos_proyectos:
-    print("ut hace parte de un proyecto")
+if proyecto_1 >= unicos_proyectos and proyecto_1 <= unicos_proyectos1:
+    print("ut hace parte de un proyecto")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
 else:
-    print("ut no camella aqui mas bien abrace del parche")
+    print(name, "ut no camella aqui mas bien abrace del parche")
 
 if proyecto_1 == 1:
-    print("ut pertenece al proyecto A, por lo tanto su sueldo es de:", salario_A)
+    print("ut pertenece al proyecto A, por lo tanto su salario es mayor a 1'500.000:", salario_A)
 
 if proyecto_1 == 2:
-    print("ut pertenece al proyecto B, por lo tanto su sueldo es de:", salario_B)
+    print("ut pertenece al proyecto B, por lo tanto su salario es mayor a 1'500.000:", salario_B)
 
 if proyecto_1 == 3:
-    print("ut pertenece al proyecto C, por lo tanto su sueldo es de:", salario_C)
+    print("ut pertenece al proyecto C, por lo tanto su salario es menor a 1'500.000:", salario_C)
